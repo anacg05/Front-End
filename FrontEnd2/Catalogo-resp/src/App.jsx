92d989374bb556d394f823d515e60e84b28b6239
anacg05 <anagc09@outlook.com>
2026-02-25 14:17:38 -0300

    .

---
+++ FrontEnd2/Catalogo-resp/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+
+import "./App.css"
+
+
+export default function App() {
+  return (
+    <div className="page">
+      <Header />
+      <main className="container">
+        <Banner />
+        <Features />
+      </main>
+      <Footer />
+    </div>
+  );
+}
